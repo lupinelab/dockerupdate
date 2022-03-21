23854c858a472e6898a60134180ac861472aacc6
lupinelab <jed@lupinelab.co.uk>
2022-03-21 14:27:56 +0000

    first commit

---
+++ dockerupdate.py
@@ -0,0 +1,43 @@
+import argparse
+from os import listdir
+import subprocess
+
+parser = argparse.ArgumentParser(description='Update all docker images and rebuild containers')
+parser.add_argument("-s", "--single", type=str, nargs=1, help="update single container/image", metavar="CONTAINER")
+args = parser.parse_args()
+
+def update(docker):
+    print(f"Stopping {docker} container:")
+    subprocess.run(["docker", "stop", docker])
+    print(f"Removing {docker} container:")
+    subprocess.run(["docker", "rm", docker])
+    print(f"Removing current {docker} image:")
+    with open(f"/home/jedrw/dockercreate/{docker}", "r") as dockercreatefile:
+        dockerregistry = (dockercreatefile.readlines()[-1].strip("\n").strip())
+    subprocess.run(["docker", "rmi", dockerregistry])
+    if docker == "vmupdown":
+        print(f"Removing base image:")
+        subprocess.run(["docker", "rmi", "ubuntu/apache2"])
+        print(f"Building {docker} image:")
+        subprocess.run(["docker", "build", "-t", "lupinelab/vmupdown", "/home/jedrw/docker/vmupdown"])
+        print(f"Creating {docker} container:")
+        subprocess.run(["sh", f"/home/jedrw/dockercreate/{docker}"])
+        print(f"Starting {docker} container:")
+        subprocess.run(["docker", "start", docker])
+        print(f"{docker} has started!")
+    else:
+        print(f"Pulling latest {docker} image:")
+        subprocess.run(["docker", "pull", dockerregistry])
+        print(f"Creating {docker} container:")
+        subprocess.run(["sh", f"/home/jedrw/dockercreate/{docker}"])
+        print(f"Starting {docker} container:")
+        subprocess.run(["docker", "start", docker])
+        print(f"{docker} has started!")
+
+
+if args.single:
+    update(args.single[0])
+else:
+    dockers = listdir("/home/jedrw/dockercreate")
+    for docker in dockers:
+        update(docker)
