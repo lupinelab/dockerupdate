82dc2da320c329122463e218cf2767495d574df1
jedrw <jed@lupinelab.co.uk>
2022-06-27 01:12:10 +0100

    cp instead of mv files & updated Pipfile

--- install.py
+++ install.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 import os
+import shutil
 import stat
 
-# make dockerupdate executeable and move to /usr/local/bin
-try:
-    os.chmod('dist/dockerupdate', os.stat('dist/dockerupdate').st_mode | stat.S_IXOTH)
-    os.rename('dist/dockerupdate', '/usr/local/bin/dockerupdate')
-except PermissionError:
-    print("Please run as elevated user")
 
-# move completion script to /etc/bash_completion.d/
-try:
-    os.rename('dockerupdate_completion', '/etc/bash_completion.d/dockerupdate_completion')
-except PermissionError:
+
+def install_script():
+    # make dockerupdate executeable and move to /usr/local/bin
+    os.chmod('dist/dockerupdate', os.stat('dist/dockerupdate').st_mode | stat.S_IXOTH)
+    shutil.copy('dist/dockerupdate', '/usr/local/bin/dockerupdate')
+    # move completion script to /etc/bash_completion.d/
+    shutil.copy('dockerupdate_completion', '/etc/bash_completion.d/dockerupdate_completion')
+
+if os.getuid() != 0:
     print("Please run as elevated user")
+else:
+    install_script()
