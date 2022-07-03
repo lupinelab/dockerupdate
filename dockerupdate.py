e7dd250f2d1a2ea05c8f5c2506265c90b8e5d40b
jedrw <jed@lupinelab.co.uk>
2022-07-03 02:19:50 +0100

    fthfxjy

--- dockerupdate.py
+++ dockerupdate.py
@@ -39,7 +39,7 @@ def update_image(container):
         imageid = subprocess.run(["docker", "images", "-q", registry], capture_output=True, text=True).stdout.strip("\n")
     print(f"{registry} - {imageid}:")
     stdout = PIPE, 
-    remove_image = subprocess.run(["docker", "rmi", "-f", imageid], stderr=STDOUT, shell=True)
+    remove_image = subprocess.run(["docker", "rmi", "-f", imageid], stdout=PIPE, stderr=STDOUT, shell=True)
     #remove_image = subprocess.run(["docker", "rmi", "-f", imageid], capture_output=True, text=True)
     while remove_image.poll() is None:
         line = remove_image.stdout.readline()
